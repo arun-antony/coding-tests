b937b13dee1c545b2a11520c1c872097d6f9a612
Arun <anton.arun@gmail.com>
2024-09-11 09:12:14 -0500

    updated ui test

--- app/src/androidTest/java/com/example/sampleapplication/feature_worldbank/presentation/ArchiveListScreenKtTest.kt
+++ app/src/androidTest/java/com/example/sampleapplication/feature_worldbank/presentation/ArchiveListScreenKtTest.kt
@@ -3,6 +3,7 @@ package com.example.sampleapplication.feature_worldbank.presentation
 import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import com.example.sampleapplication.commons.network.NetworkState
 import org.junit.Rule
 import org.junit.Test
 
@@ -15,7 +16,7 @@ class ArchiveListScreenKtTest{
         val viewModel = ArchiveViewModel()
 
         composeTestRule.setContent {
-            ArchiveListScreen(viewModel = viewModel, showSnackBar = {})
+            ArchiveListScreen(viewModel = viewModel, networkState = NetworkState.Connected, showSnackBar = {})
         }
 
         composeTestRule.onNodeWithText("ABC").isDisplayed()
